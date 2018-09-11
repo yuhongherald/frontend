ed44c12171a1365e45ae9d18782cceb39eb910b8
Yu Hong Herald <yuhongherald@gmail.com>
2018-09-11 16:55:17 +0800

    partial form

--- client/src/components/Events/EventsByPage.jsx
+++ client/src/components/Events/EventsByPage.jsx
@@ -1,7 +1,14 @@
 import React, {PropTypes} from 'react';
-import '../Events/css/Events.css'
+import '../Events/css/Events.css';
+//import '../Events/css/style.css';
 import {events} from '../Events/data.js';
 import axios from 'axios';
+
+import { render } from 'react-dom';
+import Modal from 'react-modal';
+import SlidingPane from 'react-sliding-pane';
+import 'react-sliding-pane/dist/react-sliding-pane.css';
+import Filters from './Filters.jsx';
 
 class EventsByPage extends React.Component {
 
@@ -10,10 +17,15 @@ class EventsByPage extends React.Component {
         this.state = {
             events: false,
             error: false,
-            activePage: this.props.activePage
+            activePage: this.props.activePage,
+            isPaneOpen: false,
+            isPaneOpenLeft: false
         }
         this.getData = this.getData.bind(this);
+    }
 
+    componentDidMount() {
+        Modal.setAppElement(this.el);
     }
 
     getData() {
@@ -35,7 +47,7 @@ class EventsByPage extends React.Component {
         //             error: error.desc
         //         })
         //     })
-        
+
         this.setState({
             events: events
         })
@@ -51,13 +63,32 @@ class EventsByPage extends React.Component {
         return (
             <div>
                 <div>
-                {this.getForm()}
+                {this.getSlidingPane()}
                 </div>
                 <div>
                 {this.getFormattedEventStatus()}
                 </div>
             </div>
         )
+    }
+
+    getSlidingPane() {
+        return <div ref={ref => this.el = ref}>
+            <button onClick={() => this.setState({ isPaneOpen: true })}>Click me to open right pane!</button>
+            <SlidingPane
+                className='some-custom-class'
+                overlayClassName='some-custom-overlay-class'
+                isOpen={ this.state.isPaneOpen }
+                title='Filters'
+                from='left'
+                subtitle='Optional subtitle.'
+                onRequestClose={ () => {
+                    // triggered on "<" on left top click or on outside click
+                    this.setState({ isPaneOpen: false });
+                } }>
+                <Filters/>
+            </SlidingPane>
+        </div>;
     }
 
     getFormattedEventStatus() {
@@ -76,14 +107,86 @@ class EventsByPage extends React.Component {
 
     getForm() {
         const form =
-        <div id="custom-search-input">
-            <div className="input-group col-xs-12 col-sm-12 col-md-12">
-                <input type="text" className="  search-query form-control" placeholder="Event"></input>
-								<span className="input-group-btn"><button className="btn btn-danger" type="button">
+            <div className="card card-body">
+                <div className="content-right col-md-3 z10">
+                    <div className="sidebar-searchbar col-md-12 pad0 col-sm-12">
+                        <h4>FIND AN EVENT</h4>
+                        <div id="custom-search-input">
+                            <div className="input-group col-xs-12 col-sm-12 col-md-12">
+                                <input type="text" className="  search-query form-control" placeholder="Event"/>
+                                <span className="input-group-btn"><button className="btn btn-danger" type="button">
 										<span className=" glyphicon glyphicon-search"></span>
 									</button></span>
+                            </div>
+                        </div>
+                    </div>
+                    <p>
+                        <button className="pink-borderless mobile-width btn non-mobile-hide" type="button"
+                                data-toggle="collapse" data-target="#search" aria-expanded="false"
+                                aria-controls="search">
+                            Advanced search
+                        </button>
+                    </p>
+
+                    <div className="collapse light-pink" id="search">
+                        <br/>
+                        <div className="height-auto sidebar-location col-md-12 pad0 col-sm-12">
+                            <h4>LOCATION</h4>
+                            <div id="custom-search-input">
+                                <div className="input-group col-xs-12 col-sm-12 col-md-12 absolute width-80">
+                                    <input type="text" className="  search-query form-control"
+                                           placeholder="search for location"/>
+                                    <span className="input-group-btn"><button className="btn btn-danger" type="button">
+														<span className=" glyphicon glyphicon-search"></span>
+													</button></span>
+                                </div>
+                            </div>
+                            <div className="map" id="map"></div>
+                        </div>
+                        <div className="sidebar-selectdate col-md-12 pad0 col-sm-12">
+                            <h4>SELECT DATE</h4>
+                            <div className="dates">
+                                <input className="form-control date1" id="date1" name="date" placeholder="07/15/2016"
+                                       type="text"></input>
+                                    <input className="form-control date2" id="date2" name="date"
+                                           placeholder="07/15/2016" type="text"></input>
+                            </div>
+                        </div>
+                        <div className="sidebar-popularcategories col-md-12 pad0 col-sm-12 mobile-hidden">
+                            <div className="col-md-12 popularcategories-item border-btm">
+                                <a href=""><h5>Today</h5></a>
+                            </div>
+                            <div className="col-md-12 popularcategories-item border-btm">
+                                <a href=""></a>
+                                <h5>Tomorrow</h5>
+                            </div>
+                            <div className="col-md-12 popularcategories-item border-btm">
+                                <a href=""><h5>This Week</h5></a>
+                            </div>
+                            <div className="col-md-12 popularcategories-item border-btm">
+                                <a href=""><h5>This Weekend</h5></a>
+                            </div>
+                            <div className="col-md-12 popularcategories-item border-btm">
+                                <a href=""><h5>Next Week</h5></a>
+                            </div>
+                            <div className="col-md-12 popularcategories-item">
+                                <a href=""><h5>This Month</h5></a>
+                            </div>
+                        </div>
+                        <div className="genre sidebar-popularcategories col-md-12 pad0 col-sm-12">
+
+                            <h4>TYPE</h4>
+                            <div className="col-md-12 popularcategories-item genre-type border-btm">
+                                <h5><label>
+                                    <div className="custom-checkbox">
+                                    </div>
+                                </label>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         return form;
     }
 
@@ -92,7 +195,7 @@ class EventsByPage extends React.Component {
             <div key={event.pk}
                  className="col-md-12 col-sm-12 col-xs-12 the-artist the-artist-horizontal events-page-list pad0 m-bot60">
                 <a href="events-details.html"><img
-                    src="assets/images/latestblog-img.png"
+                    src="../assets/images/latestblog-img.png"
                     className="img-responsive"/></a>
                 <div className="text-slider3">
                     <div>
